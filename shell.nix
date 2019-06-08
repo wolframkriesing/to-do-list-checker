3fd7ba0fbc9fb6c3504e211f82970b67fabb6dab
wkriesing <w+github@kriesing.de>
2019-06-08 16:48:19 +0200

    Moved to node11.

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
-with import (fetchTarball https://github.com/nixos/nixpkgs/tarball/6a3f5bcb061e1822f50e299f5616a0731636e4e7) { };
+with import (fetchTarball https://github.com/nixos/nixpkgs/tarball/f52505fac8c82716872a616c501ad9eff188f97f) { };
 
 stdenv.mkDerivation {
     name = "dev-shell";
     src = null;
-    buildInputs = [ nodejs-10_x ];
+    buildInputs = [ nodejs-11_x ];
 }
