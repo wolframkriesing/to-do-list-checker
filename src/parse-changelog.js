d5fc5aa9f0d88fae6fdcad33a2f782b4d3ac1fc6
wolframkriesing <wolfram+github@kriesing.de>
2020-03-15 16:20:52 +0100

    Ensure version is always a number!

--- src/parse-changelog.js
+++ src/parse-changelog.js
@@ -23,6 +23,6 @@ export const parseChangelog = (changelogContent) => {
   }
   const versions = changelogContent.split(LINE_START_FOR_NEW_VERSION);
   const firstVersionParagraph = versions[1];
-  const version = firstVersionParagraph.split('\n')[0];
+  const version = Number.parseInt(firstVersionParagraph.split('\n')[0], 10);
   return { version, items: todoItems(firstVersionParagraph) };
 };
